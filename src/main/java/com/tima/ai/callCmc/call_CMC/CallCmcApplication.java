ed13538e21d7e442b8bbb1c0cfed66017f5d7e7e
HoiBunCa <boybka23@gmail.com>
2022-04-14 08:40:44 +0700

    init

---
+++ src/main/java/com/tima/ai/callCmc/call_CMC/CallCmcApplication.java
@@ -0,0 +1,13 @@
+package com.tima.ai.callCmc.call_CMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallCmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallCmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tima/ai/callCmc/call_CMC/CallCmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tima.ai.callCmc.call_CMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallCmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
